988d48121ab4feeb7660f3517792612ce30ed64b
Alexander <kaamosdao@gmail.com>
2022-11-30 13:48:21 +0300

    initial

---
+++ webpack.config.cjs
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  resolve: {
+    extensions: ['.js'],
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist/public'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devServer: {
+    port: 4242,
+  },
+  devtool: mode === 'development' ? 'source-map' : false,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'assets/img'),
+          to: path.resolve(__dirname, 'dist/public/assets/img'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer'],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/img/[name][ext]',
+        },
+     }
+    ],
+  },
+};
